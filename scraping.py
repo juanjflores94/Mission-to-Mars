33ae76b8d8cda0d1b6d3f9216689b3662a875b89
Juan <jayjay>
2022-06-06 21:48:54 -0700

    Updated files

--- scraping.py
+++ scraping.py
@@ -22,9 +22,7 @@ data = {
       "last_modified": dt.datetime.now()
 }
 
-    # Stop webdriver and return data
-    browser.quit()
-    return data
+   
 
 
 def mars_news(browser):
@@ -32,17 +30,17 @@ def mars_news(browser):
     # Scrape Mars News
 
 # Visit the mars nasa news site
-url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
-browser.visit(url)
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    browser.visit(url)
 
 
 # Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
 
 # Convert the browser html to a soup object
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
 
 # Add try/except for error handling
@@ -65,18 +63,18 @@ return news_title, news_p
 def featured_image(browser):
 
 # Visit URL
-url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-browser.visit(url)
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
 
 
 # Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
 
 # Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
 
 # Add try/except for error handling
@@ -100,14 +98,15 @@ def mars_facts():
 
         except BaseException:
             return None
+        
 # Assign columns and set index of dataframe
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
 
 # Convert dataframe into HTML format, add bootstrap
-return df.to_html()
+    return df.to_html()
 
-if__name__=="__main__":
+    if__name__=="__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
